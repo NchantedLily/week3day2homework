8fe6f51958d7839b82a28759be1d45260bad378c
Martia <pureromancebymartia@gmail.com>
2022-04-12 19:22:40 -0400

    welcome

---
+++ app.js
@@ -0,0 +1,42 @@
+var a=10;
+var b=20;
+
+function addSum(param1, param2){
+
+
+console.log(param1 + param2);
+
+}
+addSum(a,b);
+
+
+var a=5;
+var b=10;
+
+function subtractSum(param1, param2){
+    console.log(param1 - param2);
+
+}
+subtractSum(a,b);
+
+
+var a=15;
+var b=3;
+
+function multiplySum(param1, param2){
+    console.log(param1 * param2);
+}
+multiplySum(a,b);
+
+
+
+var a=100;
+var b=20;
+
+function divideSum(param1,param2){
+    console.log(param1 / param2);
+}
+divideSum(a,b);
+
+
+
